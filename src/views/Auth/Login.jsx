b3003698969a923d7ae04c327ad215e04e5aec29
m-ghiringhelli <mghiring@gmail.com>
2022-05-04 12:37:27 -0700

    login works and /treehouse/members is protected

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -18,7 +18,11 @@ export default function Login() {
   const handleLogin = (event) => {
     event.preventDefault();
     const loginWasSuccessful = auth.login(formState.email, formState.password);
-
+    if (loginWasSuccessful) {
+      history.replace(from);
+    } else {
+      setError('login failed, loser');
+    }
     // TODO: If login was unsuccessful, set an error with a message
     // to display to the user that their login failed.
     //
@@ -32,17 +36,21 @@ export default function Login() {
     <>
       <h3>You must log in to view the page at {from.pathname}</h3>
       <form onSubmit={handleLogin} className={styles.loginForm}>
-        <label>Email</label>
+        <label htmlFor='email'>Email</label>
         <input
           id="email"
           name="email"
           type="email"
+          value={formState.email}
+          onChange={(e) => handleFormChange(e)}
         />{' '}
-        <label>Password</label>
+        <label htmlFor='password'>Password</label>
         <input
           id="password"
           name="password"
           type="password"
+          value={formState.password}
+          onChange={(e) => handleFormChange(e)}
         />
         <button type="submit" aria-label="Sign In">
           Sign in
